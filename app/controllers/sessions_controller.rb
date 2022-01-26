765f550142130ad1f7db2984d9533698f4f3a6c2
Clementina Mendes <clementinadesmen@gmail.com>
2022-01-26 21:54:12 +0100

    Finish login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = "Félicitations pour cette incroyable connexion !"
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Aurevoir !"
+    redirect_to root_path
+  end
+end
